40d732ab4911b920c159e6ae6619fa1ce81645a2
Felipe Dalle Molle <dallemollefelipe@gmail.com>
2019-12-03 22:30:52 -0300

    AutorExistenteException

--- src/main/java/com/algaworks/socialbooks/resources/AutoresResource.java
+++ src/main/java/com/algaworks/socialbooks/resources/AutoresResource.java
@@ -1,13 +1,16 @@
 package com.algaworks.socialbooks.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.algaworks.socialbooks.domain.Autor;
 import com.algaworks.socialbooks.services.AutoresService;
@@ -23,6 +26,16 @@ public class AutoresResource {
 	public ResponseEntity<List<Autor>> listar() {
 		List<Autor> autores = autoresService.listar();
 		return ResponseEntity.status(HttpStatus.OK).body(autores);
+	}
+	
+	@RequestMapping(method = RequestMethod.POST)
+	public ResponseEntity<Void> salvar(@RequestBody Autor autor) {
+		autor = autoresService.salvar(autor);
+		
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+				.path("/{id}").buildAndExpand(autor.getId()).toUri();
+		
+		return ResponseEntity.created(uri).build();
 	}
 
 }
--- src/main/java/com/algaworks/socialbooks/services/AutoresService.java
+++ src/main/java/com/algaworks/socialbooks/services/AutoresService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.algaworks.socialbooks.domain.Autor;
 import com.algaworks.socialbooks.repository.AutoresRepository;
+import com.algaworks.socialbooks.services.exceptions.AutorExistenteException;
 
 @Service
 public class AutoresService {
@@ -16,6 +17,18 @@ public class AutoresService {
 	
 	public List<Autor> listar(){
 		return autoresRepository.findAll();
+	}
+	
+	public Autor salvar(Autor autor) {
+		
+		if(autor.getId() != null) {
+			Autor a = autoresRepository.findById(autor.getId()).orElse(null);
+
+			if(a != null) {
+				throw new AutorExistenteException("O autor já existe.");
+			}
+		}
+		return autoresRepository.save(autor);
 	}
 
 }
